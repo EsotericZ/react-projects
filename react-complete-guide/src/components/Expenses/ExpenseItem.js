59033becae547370ad9bb1b4edb62dbf6019dfcc
EsotericZ <cjsand03@gmail.com>
2022-03-18 12:44:50 -0600

    updated udemy projecy

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,10 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import Card from '../UI/Card';
 
-function ExpenseItem(props) {
-
+const ExpenseItem = (props) => {
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate
                 date={props.date}
             />
@@ -12,7 +12,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
