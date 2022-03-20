c11351cdfc58de1b4babf052ac3753fd5f7be7f2
EsotericZ <cjsand03@gmail.com>
2022-03-20 10:24:21 -0600

    completed expenses

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import './Expenses.css';
 
@@ -9,8 +10,12 @@ const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = selectedYear => {
-        filteredYear(selectedYear);
+        setFilteredYear(selectedYear);
     };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <div>
@@ -19,26 +24,9 @@ const Expenses = (props) => {
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler}
                 />
-                <ExpenseItem
-                    title={props.items[0].title} 
-                    amount={props.items[0].amount} 
-                    date={props.items[0].date} 
+                <ExpensesList 
+                    items={filteredExpenses}
                 />
-                <ExpenseItem
-                    title={props.items[1].title} 
-                    amount={props.items[1].amount} 
-                    date={props.items[1].date} 
-                />
-                <ExpenseItem
-                    title={props.items[2].title} 
-                    amount={props.items[2].amount} 
-                    date={props.items[2].date} 
-                />
-                <ExpenseItem
-                    title={props.items[3].title} 
-                    amount={props.items[3].amount} 
-                    date={props.items[3].date} 
-                /> 
         </Card>
       </div>
     )
